6ad35f7df4812dd2c268f417a6d1b3d455652e3c
MarcoCaamal <caamalmarco99@gmail.com>
2025-03-27 14:17:24 -0500

    Update app name

--- resources/views/layouts/app-layout.blade.php
+++ resources/views/layouts/app-layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>AsesoraT - @yield('title')</title>
+    <title>Aulapp - @yield('title')</title>
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
